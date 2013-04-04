80a0a28fcf005a504e19c5e2ac4dfa740ed4ae6f
Neil Kelty <neil@neilkelty.com>
2013-04-04 08:55:33 -0400

    Add test for email with consecutive '..' to User model spec. Complete Exercise 6-5-3.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addreses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+      addreses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addreses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
