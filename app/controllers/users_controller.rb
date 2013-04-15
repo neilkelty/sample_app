5b83381a0b2c6aefd89519b2004ccce88dc5a498
Neil Kelty <neil@neilkelty.com>
2013-04-15 08:12:05 -0400

    Add some Tomdoc to the users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,33 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
+  # Internal: Display paginated view of all users of the application
+  # 
+  # Returns a paginated hash of all users and their attributes
   def index
     @users = User.paginate(page: params[:page])
   end
 
+  # Internal: Display details and attributes of a single user
+  # 
+  # Returns a single user and a hash of all their microposts
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
 
+  # Internal: Instaniate a new user object
+  # 
+  # Returns new user object
   def new
     @user = User.new
   end
 
+  # Public: Create a new user
+  # 
+  # user_params - Information about the user to store in the database
+  # 
+  # Creates a new user in the database
   def create
     @user = User.new(user_params)
     if @user.save
